a1e6a6c5bc4a0631487164ab59c7a037c67c3c54
yoginegi <69087517+yoginegi@users.noreply.github.com>
2024-10-30 17:46:48 -0500

    Average Time of Process per Machine

---
+++ Ten.sql
@@ -0,0 +1,7 @@
+SELECT a1.machine_id, round(AVG(a2.timestamp- a1.timestamp),3) as processing_time
+FROM Activity a1
+INNER JOIN Activity a2
+ON a1.process_id=a2.process_id 
+AND a1.machine_id=a2.machine_id 
+AND a1.timestamp<a2.timestamp
+GROUP BY a1.machine_id
