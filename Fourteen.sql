10230fa40c33c9d09fbbe6f6cd8c4b385b066b1d
yoginegi <69087517+yoginegi@users.noreply.github.com>
2024-11-04 14:34:15 -0600

    Confirmation Rate

---
+++ Fourteen.sql
@@ -0,0 +1,5 @@
+SELECT s.user_id, IFNULL(ROUND(SUM(c.action = 'confirmed')/COUNT(*),2) , 0.00) AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c
+ON s.user_id = c.user_id
+GROUP BY s.user_id
