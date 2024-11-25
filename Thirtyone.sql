5b99b69f268b16438057926d917e130d73c92fc6
yoginegi <69087517+yoginegi@users.noreply.github.com>
2024-11-25 00:07:04 -0600

    Primary Department for Each Employee

---
+++ Thirtyone.sql
@@ -0,0 +1,7 @@
+SELECT DISTINCT employee_id, department_id
+FROM Employee
+WHERE employee_id IN (SELECT  employee_id
+FROM Employee
+GROUP BY employee_id
+HAVING COUNT(*) = 1)
+OR primary_flag = 'Y'
